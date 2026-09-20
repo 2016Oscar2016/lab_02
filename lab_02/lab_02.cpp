92690792c684f5d620bcda6f77d0cce5c5b9a3f2
2016Oscar2016 <olexandr.pasternak@gmail.com>
2026-09-20 22:35:14 +0300

    4th commit

--- lab_02/lab_02.cpp
+++ lab_02/lab_02.cpp
@@ -13,15 +13,15 @@ int main()
 {
     double Pi = 4 * atan(1.);   // число пі
     double a;                   // кут у радіанах
-    //double z1;                  // результат обчислення 1-го виразу
-    //double z2;                  // результат обчислення 2-го виразу
+    double z1;                  // результат обчислення 1-го виразу
+    double z2;                  // результат обчислення 2-го виразу
 
     cout << "a = ";
     cin >> a;
 
-    //z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * pow(sin(2 * a), 2));
+    z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * pow(sin(2 * a), 2));
 
-    //z2 = 2 * sin(a);
+    z2 = 2 * sin(a);
 
     cout << "z1 = " << z1 << endl;
     cout << "z2 = " << z2 << endl;
